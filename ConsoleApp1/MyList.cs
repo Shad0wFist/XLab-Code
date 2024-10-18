7f7511a0199608b5a924a0f80b48f941bd586d2a
Ilya <ilya.starkov03@gmail.com>
2024-10-18 18:07:13 +0300

    Comm

---
+++ ConsoleApp1/MyList.cs
@@ -0,0 +1,43 @@
+public class MyList
+{
+    private int[] array = new int[4];
+
+
+    public void Add(int item)
+    {
+        
+    }
+
+    public void Delete(int item) // первое вхождение
+    {
+
+    }
+
+    public void Insert(int item)
+    {
+
+    }
+
+    public void Clear(int item)
+    {
+
+    }
+    
+    public void ToString(int item)
+    {
+
+    }
+
+    public void DeleteAt (int index, int item) //по индексу
+    {
+
+    }
+
+    public void DeleteAll (int item)
+    {
+
+    }
+
+
+    
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,8 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int i = 0;
+        long l = (long)int.MaxValue + 3;
 
+        Console.WriteLine($"l = {l}");
+
+        i = (int)l;
+
+        Console.WriteLine($"i = {i}");
         var result = Console.ReadLine();
         Console.WriteLine(result);
     }
